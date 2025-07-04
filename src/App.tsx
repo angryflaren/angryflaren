f3b7adc3360842272a90a70c07138f8092d37637
Zhuchkov Matthew <136967840+angryflaren@users.noreply.github.com>
2025-07-04 18:01:04 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { FC, useState, useEffect } from 'react';
-import { I18nextProvider } from 'react-i18next';
+import { I18nextProvider, useTranslation } from 'react-i18next';
 import i18n from './i18n/config';
 import { Resume as ResumeComponent } from './components/Resume';
 import type { ResumeSchema } from './types/resumeSchema';
-import resumeEn from '../resume.json';
+import resumeEn from '../resume.en.json';
 import resumeRu from '../resume.ru.json';
 
 const App: FC = () => {
+  const { t } = useTranslation();
   const [lang, setLang] = useState<'en' | 'ru'>('ru');
-  const [resume, setResume] = useState<ResumeSchema>(lang === 'ru' ? resumeRu : resumeEn);
+  const [resume, setResume] = useState<ResumeSchema>(resumeRu);
 
   const handleLanguageChange = (newLang: 'en' | 'ru') => {
     setLang(newLang);
@@ -17,7 +18,8 @@ const App: FC = () => {
   useEffect(() => {
     i18n.changeLanguage(lang);
     setResume(lang === 'ru' ? resumeRu : resumeEn);
-  }, [lang]);
+    document.title = t('meta.title');
+  }, [lang, t]);
 
   return (
     <I18nextProvider i18n={i18n}>
